5734e3386f3df754593f0d6cadf9293b22f0791c
mcbianconi <murillo.bianconi@gmail.com>
2020-06-04 21:27:44 -0300

    front cleanup

--- web_react/src/App.tsx
+++ web_react/src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>selva</h2>
-      </header>
-    </div>
+    <h1>selva</h1>
   );
 }
 
